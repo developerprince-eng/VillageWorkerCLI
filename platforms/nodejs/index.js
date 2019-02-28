a8144b9e378327373cd4597cf632dcfd87652a75
Prince Maposa <princekudzaimaposa94@gmail.com>
2019-02-28 10:12:20 +0200

    changes

--- platforms/nodejs/index.js
+++ platforms/nodejs/index.js
@@ -7,16 +7,42 @@ let cmds = {
 }
 
 let options = {
-    version: `--version` || `-v`
+    version: `--version` || `-v`,
+    angular: `install -g @angular/cli`,
+    newng: `ng new my-app`,
+    react:`npx create-react-app`
 }
+
 
 let winev = /^win/.test(process.platform) ? 'git.npm' : cmds.npm
 
-let child = cp.spawn( winev || cmds.npm, [options.version]);
+let angular = (name)=> {
+    let child = cp.spawn( winev || cmds.npm, [options.angular])
+    
+    child.stdout.on(`data`, (data) => {
+        console.log(`progress: \n${data}`)
+    })
 
-child.stdout.on("data", (data) => {
-    console.log(`data:\n${data}`)
-})
-// const { spawn } = require('child_process')
+    let child2 = cp.spawn( winev || cmds.npm, [options.newng, name])
 
-// const child = spawn(/^win/.test(process.platform) ? 'npm.cmd' : 'npm', ['-v']);
+    child2.stdout.on(`data`, (data) => {
+        console.log(`new angular stepup: \n${data}`)
+    })
+
+}
+
+exports.ndjs =  { angular }
+
+let react = (name)=> {
+    let child = cp.spawn( winev || cmds.npm, [options.react, name])
+
+    child.stdout.on(`data`, (data) => {
+        console.log(`progress: \n${data}`)
+    })
+
+    let child2 = cp.spawn( winev || cmds.npm, [options.react, name])
+
+    child2.stdout.on(`data`, (data) => {
+        console.log(`new reactapp steup: \n${data}`)
+    })
+}
--- test.js
+++ test.js
@@ -1,11 +1,15 @@
-const path = require('path')
-const fs = require('fs');
+//  const path = require('path')
+// const fs = require('fs');
 
-let messages_path = path.join(__dirname, `/messages`, `/welcome.text`);
-console.log(messages_path);
+// let messages_path = path.join(__dirname, `/messages`, `/welcome.text`);
+// console.log(messages_path);
 
-console.log(typeof messages_path);
-fs.readFile(messages_path , 'utf8' ,(err, data) => {
-    if(err) throw err;
-    console.log(data);
-})
+// console.log(typeof messages_path);
+// fs.readFile(messages_path , 'utf8' ,(err, data) => {
+//     if(err) throw err;
+//     console.log(data);
+// })
+
+let ng6 = require(`./platforms/nodejs`)
+
+ng6.ndjs.angular(`prince`)
