32e9c78e2bc0a35bff30bce5aca553c8aaff8064
Prince Maposa <princekudzaimaposa94@gmail.com>
2019-03-01 18:11:52 +0200

    changes

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const nd = require(`./platforms/nodejs/index`);
 
 program
     .version(`1.0.0`, `-v, --version`);
-    .description(`Project Scafolding for Project`);
 
 
 //This is for Scafolding a project
@@ -17,7 +16,8 @@ program
     .option(`-g --generatee`, `Trigger Generation Operation`)
     .option(`--project -p`)
     .action((name) => {
-         console.log(`\x1b[36m%s\x1b[0m`, name, `Web Application Scafolded Successfully...\n`,
+        nd.ndjs.emptypro(name);
+        console.log(`\x1b[36m%s\x1b[0m`, name, `Web Application Scafolded Successfully...\n`,
         `\x1b[33m`,
         `Enjoy!!!`,
         `\x1b[0m`);
@@ -41,7 +41,7 @@ program
     .option(`-p --platform <platform>`, `State the Platform of development`)
     .option(`--view -v <view>`, `Seletct View Templating Engine`)
     .action( (name, view, platform) => {
-        if(platform == `nodejs`) => {
+        if(platform == `nodejs`) {
             if(view == `angular`){
                 nd.ndjs.angular(name, view);
             }
--- platforms/nodejs/index.js
+++ platforms/nodejs/index.js
@@ -1,6 +1,6 @@
-let cp = require(`child_process`);
+const cp = require(`child_process`);
 
-let cmds = {
+const cmds = {
     bash: `bash`,
     cmd: `cmd`,
     npm: `npm`,
@@ -8,95 +8,336 @@ let cmds = {
     heroku: `heroku`
 };
 
-let options = {
+const options = {
     version: `--version` || `-v`,
     angular: `install -g @angular/cli`,
     newng: `ng new`,
     newreact:`npx create-react-app`,
     vue: `i create-vue-app -g`,
-    newvue: `create-vue-app`
+    newvue: `create-vue-app`,
+    mobile: `install -g ionic`,
+    newmobile: `ionic start`
 };
 
-let folderOptions = {
+const folderOptions = {
     model: `mkdir models`,
     view: `mkdir views`,
     controller: `mkdir controllers`
 };
 
-let models = {
+const models = {
     user: `type nul> models/user.model.js`,
     password: `type nul> modes/password.model.js`,
     log: `type nul> models/logs.model.js`
 }
 
-let controllers = {
+const controllers = {
     user: `type nul> controllers/user.controller.js`,
     password: `type nul> contrllers/password.controller.js`,
     log: `type nul> controllers/logs.controller.js`
 }
 
 //Success Console Message 
-let websuccess = ()=> {
+const websuccess = (name)=> {
     console.log(`\x1b[36m%s\x1b[0m`, name, `Web Application Scafolded Successfully...\n`,`\x1b[33m`,`Enjoy!!!`,`\x1b[0m`);
 } 
 
 //Check for Winows Environment
-let winev = /^win/.test(process.platform) ? `npm.cmd` : cmds.npm;
-let winev2 = /^win/.test(process.platform) ? `cmd` : cmds.npm;
+const isWin = process.platform === `win32`;
+
+const isUnix = process.platform === `darwin`;
 
 //Empty project Scafold
 let emptypro = (name) => {
-    let child1 = cp.spawn( winev2 || cmds.bash, [folderOptions.model]);
-    let child2 = cp.spawn( winev2 || cmds.bash, [folderOptions.view]);
-    let child3 = cp.spawn( winev2 || cmds.bash, [folderOptions.controller]);
-    let child4 = cp.spawn( winev2 || cmds.bash, [models.user]);
-    let child5 = cp.spawn( winev2 || cmds.bash, [models.password]);
-    let child6 = cp.spawn( winev2 || cmds.bash, [models.log]);
+    if (isWin) {
+        let child1 = cp.spawn( `cmd.exe` , [folderOptions.model]);
 
-    websuccess();
+    
+        child1.stderr.on(`data`, () => {
+            console.log(`Error occured please check`);
+        }); 
+    
+        child1.stdout.on(`data`, (data) => {
+            console.log(`Added: \n${data}`);
+        });
+    
+    
+        let child2 = cp.spawn( `cmd.exe`, [folderOptions.view]);
+    
+        
+        child2.stderr.on(`data`, () => {
+            console.log(`Error occured please check`)
+        }); 
+    
+        child2.stdout.on(`data`, (data) => {
+            console.log(`Added: \n${data}`);
+        });
+    
+        let child3 = cp.spawn( `cmd.exe`, [folderOptions.controller]);
+    
+        
+        child3.stderr.on(`data`, () => {
+            console.log(`Error occured please check`);
+        }); 
+    
+        child3.stdout.on(`data`, (data) => {
+            console.log(`Added: \n${data}`);
+        });
+    
+        let child4 = cp.spawn( `cmd.exe` , [models.user]);
+    
+        
+        child4.stderr.on(`data`, () => {
+            console.log(`Error occured please check`);
+        }); 
+    
+        child4.stdout.on(`data`, (data) => {
+            console.log(`Added: \n${data}`);
+        });
+    
+        let child5 = cp.spawn( `cmd.exe` , [models.password]);
+    
+        
+        child5.stderr.on(`data`, () => {
+            console.log(`Error occured please check`);
+        }); 
+    
+        child5.stdout.on(`data`, (data) => {
+            console.log(`Added: \n${data}`);
+        });
+    
+        let child6 = cp.spawn( `cmd.exe` , [models.log]);
+        
+        child6.stderr.on(`data`, () => {
+            console.log(`Error occured please check`);
+        }); 
+    
+        child6.stdout.on(`data`, (data) => {
+            console.log(`Added: \n${data}`);
+        });
+    
+        let child7 = cp.spawn( `cmd.exe` , [controllers.user]);
+    
+        child7.stderr.on(`data`, () => {
+            console.log(`Error occured please check`);
+        });
+    
+        child7.stdout.on(`data`, (data) => {
+            console.log(`Added: \n${data}`);
+        });
+    
+        let child8 = cp.spawn( `cmd.exe` , [controllers.password]);
+    
+        child8.stderr.on(`data`, () => {
+            console.log(`Error occured please check`);
+        });
+    
+        child8.stdout.on(`data`, (data) => {
+            console.log(`Added: \n${data}`);
+        });
+    
+        let child9 = cp.spawn( `cmd.exe`, [controllers.password]);
+    
+        child9.stderr.on(`data`, () => {
+            console.log(`Error occured please check`);
+        });
+    
+        child9.stdout.on(`data`, (data) => {
+            console.log(`Added: \n${data}`);
+        });
+    
+        websuccess(name);
+    }
+    if(isUnix) {
+        let child1 = cp.spawn( `bash` , [folderOptions.model]);
+
+    
+        child1.stderr.on(`data`, () => {
+            console.log(`Error occured please check`);
+        }); 
+    
+        child1.stdout.on(`data`, (data) => {
+            console.log(`Added: \n${data}`);
+        });
+    
+    
+        let child2 = cp.spawn( `bash`, [folderOptions.view]);
+    
+        
+        child2.stderr.on(`data`, () => {
+            console.log(`Error occured please check`)
+        }); 
+    
+        child2.stdout.on(`data`, (data) => {
+            console.log(`Added: \n${data}`);
+        });
+    
+        let child3 = cp.spawn( `bash`, [folderOptions.controller]);
+    
+        
+        child3.stderr.on(`data`, () => {
+            console.log(`Error occured please check`);
+        }); 
+    
+        child3.stdout.on(`data`, (data) => {
+            console.log(`Added: \n${data}`);
+        });
+    
+        let child4 = cp.spawn( `bash` , [models.user]);
+    
+        
+        child4.stderr.on(`data`, () => {
+            console.log(`Error occured please check`);
+        }); 
+    
+        child4.stdout.on(`data`, (data) => {
+            console.log(`Added: \n${data}`);
+        });
+    
+        let child5 = cp.spawn( `bash` , [models.password]);
+    
+        
+        child5.stderr.on(`data`, () => {
+            console.log(`Error occured please check`);
+        }); 
+    
+        child5.stdout.on(`data`, (data) => {
+            console.log(`Added: \n${data}`);
+        });
+    
+        let child6 = cp.spawn( `bash` , [models.log]);
+        
+        child6.stderr.on(`data`, () => {
+            console.log(`Error occured please check`);
+        }); 
+    
+        child6.stdout.on(`data`, (data) => {
+            console.log(`Added: \n${data}`);
+        });
+    
+        let child7 = cp.spawn( `bash` , [controllers.user]);
+    
+        child7.stderr.on(`data`, () => {
+            console.log(`Error occured please check`);
+        });
+    
+        child7.stdout.on(`data`, (data) => {
+            console.log(`Added: \n${data}`);
+        });
+    
+        let child8 = cp.spawn( `bash` , [controllers.password]);
+    
+        child8.stderr.on(`data`, () => {
+            console.log(`Error occured please check`);
+        });
+    
+        child8.stdout.on(`data`, (data) => {
+            console.log(`Added: \n${data}`);
+        });
+    
+        let child9 = cp.spawn( `bash`, [controllers.password]);
+    
+        child9.stderr.on(`data`, () => {
+            console.log(`Error occured please check`);
+        });
+    
+        child9.stdout.on(`data`, (data) => {
+            console.log(`Added: \n${data}`);
+        });
+    
+        websuccess(name);
+    }
 }
 
 //Scafold Angular Application
 let angular = (name)=> {
-    let child1 = cp.spawn( winev || cmds.npm, [options.angular]);
-    
+    let child1 = cp.spawn( `cmd.exe`, [options.angular]);
+
+    child1.stderr.on(`data`, () => {
+        console.log(`Error occured please check`)
+    }) 
+
     child1.stdout.on(`data`, (data) => {
         console.log(`progress: \n${data}`);
     });
 
-    let child2 = cp.spawn( winev || cmds.npm, [options.newng, name ]);
+    let child2 = cp.spawn( `cmd.exe`, [options.newng, name ]);
+    
+    child2.stderr.on(`data`, () => {
+        console.log(`Error occured please check`)
+    }) 
+
 
     child2.stdout.on(`data`, (data) => {
         console.log(`new angular setup: \n${data}`);
-        websuccess();
+        websuccess(name);
     });    
 };
 
 //Scafold React Application
 let react = (name)=> {
-    let child1 = cp.spawn( winev || cmds.npm, [options.react, name]);
+    let child1 = cp.spawn( `cmd.exe`, [options.react, name]);
+
+    child1.stderr.on(`data`, () => {
+        console.log(`Error occured please check`)
+    }) 
 
     child1.stdout.on(`data`, (data) => {
         console.log(`new reactapp setup: \n${data}`);
-        websuccess();
+        websuccess(name);
     });
 };
 
 //Scafold Vue Application
 let vue = (name)=> {
-    let child1 = cp.spawn( winev || cmds.npm, [options.vue]);
+    let child1 = cp.spawn( `cmd.exe`, [options.vue]);
+
+    
+    child1.stderr.on(`data`, () => {
+        console.log(`Error occured please check`)
+    }) 
+
 
     child1.stdout.on(`data`, (data) => {
         console.log(`progress: \n${data}`);
     });
 
-    let child2 = cp.spawn( winev || cmds.npm, [options.newvue, name]);
+    let child2 = cp.spawn( `cmd.exe`, [options.newvue, name]);
+
+    
+    child2.stderr.on(`data`, () => {
+        console.log(`Error occured please check`)
+    }) 
+
 
     child2.stdout.on(`data`, (data) => {
         console.log(`new vueapp setup: \n${data}`);
-        websuccess();
+        websuccess(name);
     });
 };
 
-exports.ndjs =  { angular, react , vue, emptypro}
+let mobile = (name) => {
+
+
+    let child1 = cp.spawn( `cmd.exe`, [options.mobile]);
+
+    child1.stderr.on(`data`, () => {
+        console.log(`Error Occured please check`);
+    });
+
+    child1.stdout.on(`data`, (data) => {
+        console.log(`Progress: \n${data}`);
+    });
+
+    let child2 = cp.spawn( `cmd.exe`, [options.newmobile, name]);
+
+    child2.stderr.on(`data`, () => {
+        console.log(`Error Occured please check`);
+    })
+
+    child2.stdout.on(`data`, (data) => {
+        console.log(`Setup: \n${data}`);
+    })
+}
+exports.ndjs =  { angular, react , vue, emptypro, mobile}
 
