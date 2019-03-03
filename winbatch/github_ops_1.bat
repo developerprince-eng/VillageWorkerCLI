fc398a2bea5309cab7bb9ca5083c26692d35d4ae
Prince Maposa <princekudzaimaposa94@gmail.com>
2019-03-03 12:26:47 +0200

    Change up

---
+++ winbatch/github_ops_1.bat
@@ -0,0 +1,24 @@
+ECHO Swicthing to Powershell
+call powershell
+call [environment]::setEnvironmentVariable('SCOOP','C:\Applications\Scoop','User')
+call $env:SCOOP='C:\Applications\Scoop'
+call iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
+ECHO Setting and Install Github CommandLine Interface
+call scoop install hub
+ECHO ****************************************
+ECHO  Now Lets Do Magic using Hub to Access Github
+ECHO ****************************************
+call git init 
+call git add .
+call git commit -m "Initial Commit, Using VillageWorkCLI"
+call hub create
+call git push origin
+ECHO ****************************************
+ECHO WE Have Successfully Pushed The Project 
+ECHO ______________^(* _ *)^________________
+ECHO ************* ENJOY!!! *****************
+ECHO.
+ECHO ************* HAPPY CODING *************
+call exit
+
+
