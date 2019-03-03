42dc4df3103c32888fe053b862feddbd7a6e40eb
Prince Maposa <princekudzaimaposa94@gmail.com>
2019-03-03 12:17:21 +0200

    switching it Up

--- winbatch/node_web_angular.bat
+++ winbatch/node_web_angular.bat
@@ -8,6 +8,26 @@ call npm install -g ionic
 @ECHO "Creating an Angular App: "
 call ng new %1
 ECHO ****************************************
+ECHO #####Lets Switch up things a little#####
+ECHO ****************************************
+ECHO Swicthing to Powershell
+call powershell
+call [environment]::setEnvironmentVariable('SCOOP','C:\Applications\Scoop','User')
+call $env:SCOOP='C:\Applications\Scoop'
+call iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
+ECHO Setting and Install Github CommandLine Interface
+call scoop install hub
+ECHO ****************************************
+ECHO  Now Lets Do Magic using Hub to Access Github
+ECHO ****************************************
+call git init 
+call git add .
+call git commit -m "Initial Commit, Using VillageWorkCLI"
+call hub create
+call git push origin
+ECHO ****************************************
+ECHO WE Have Successfully Pushed The Project 
+ECHO ______________^(* _ *)^________________
 ECHO ************* ENJOY!!! *****************
 ECHO.
 ECHO ************* HAPPY CODING *************
--- winbatch/node_web_react.bat
+++ winbatch/node_web_react.bat
@@ -6,6 +6,26 @@ ECHO.
 @ECHO "Creating a Reac App: "
 call npx create-react-app %1
 ECHO ****************************************
+ECHO #####Lets Switch up things a little#####
+ECHO ****************************************
+ECHO Swicthing to Powershell
+call powershell
+call [environment]::setEnvironmentVariable('SCOOP','C:\Applications\Scoop','User')
+call $env:SCOOP='C:\Applications\Scoop'
+call iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
+ECHO Setting and Install Github CommandLine Interface
+call scoop install hub
+ECHO ****************************************
+ECHO  Now Lets Do Magic using Hub to Access Github
+ECHO ****************************************
+call git init 
+call git add .
+call git commit -m "Initial Commit, Using VillageWorkCLI"
+call hub create
+call git push origin
+ECHO ****************************************
+ECHO WE Have Successfully Pushed The Project 
+ECHO ______________^(* _ *)^________________
 ECHO ************* ENJOY!!! *****************
 ECHO.
 ECHO ************* HAPPY CODING *************
