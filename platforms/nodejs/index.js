f9a3deec0d664cc34636ea1d27393ee2756c0fee
Prince Maposa <princekudzaimaposa94@gmail.com>
2019-03-06 06:27:44 +0200

    Initial Commit, Using VillageWorkCLI

--- platforms/nodejs/index.js
+++ platforms/nodejs/index.js
@@ -15,7 +15,7 @@ const isUnix = process.platform === `darwin`;
 let pwshell = () => {
     if(isWin) {
         cp.exec(`powershell`, (err, stdout, stderr) => {
-            if(err) throw err
+            if(err) throw err   
 
             if(stderr) console.log(`Kublind Baba! : \n`, stderr)
 
@@ -30,18 +30,177 @@ let emptypro = (name) => {
         cp.exec(`mkdir ${name} && cd ${name}`, (err, stdout, stderr) => {
             if(err) throw err
 
-            if(stderr) console.log(`Error occured: \n`, stderr)
+            if(stderr) console.log(stderr)
 
-            if(stdout) console.log(`Creating Root Project: Folder\n`, stdout,`\x1b[36m \nNow in Current Working Directory: \n `, `\x1b[0m` )
+            if(stdout) console.log(`Creating Root Project: Folder\n`, stdout,`\x1b[36m \nNow in Current Working Directory: \n `, `\x1b[0m`,
+            `****************************************\n`,
+            `******* CREATING A NEW PROJECT *********\n`,
+            `****************************************\n`
+            )
         })
         
-        cp.exec(`node_emptypro.bat ${name}`,  (err, stdout, stderr) => {
+        cp.exec(`mkdir ${name}\\models && mkdir ${name}\\controllers && mkdir ${name}\\views`, (err, stdout, stderr) => {
+            if(err) throw err
+
+            if(stderr) console.log(stderr)
+
+            if(stdout) console.log(`Creating Project Sub-Root Folders: \n`, stdout)
+        })
+
+        cp.exec(`type nul>${name}/models/user.models.js`, (err, stderr, stdout) => {
+            if(err) throw err
+
+            if(stderr) console.log(stderr)
+
+            if(stdout) console.log(stdout)
+        })
+
+        cp.exec(`type nul>${name}/models/password.models.js`, (err, stderr, stdout) => {
+            if(err) throw err
+
+            if(stderr) console.log(stderr)
+
+            if(stdout) console.log(stdout)
+        })
+
+        cp.exec(`type nul>${name}/models/log.models.js`, (err, stderr, stdout) => {
+            if(err) throw err
+
+            if(stderr) console.log(stderr)
+
+            if(stdout) console.log(stdout)
+        })
+
+        cp.exec(`type nul>${name}/controllers/user.controller.js`, (err, stderr, stdout) => {
+            if(err) throw err
+
+            if(stderr) console.log(stderr)
+
+            if(stdout) console.log(stdout)
+        })
+
+        // cp.exec(`type nul>${name}/controllers/password.controller.js`, (err, stderr, stdout) => {
+        //     if(err) throw err
+
+        //     if(stderr) console.log(stderr)
+
+        //     if(stdout) console.log(stdout)
+        // })
+
+        cp.exec(`type nul>${name}/controllers/logs.controller.js`, (err, stderr, stdout) => {
+            if(err) throw err
+
+            if(stderr) console.log(stderr)
+
+            if(stdout) console.log(stdout)
+        })
+
+        cp.exec(`type nul>${name}/controllers/logs.controller.js`, (err, stderr, stdout) => {
+            if(err) throw err
+
+            if(stderr) console.log(stderr)
+
+            if(stdout) console.log(stdout)
+        })
+
+        cp.exec(`type nul>${name}/views/signin.view.ejs`, (err, stderr, stdout) => {
+            if(err) throw err
+
+            if(stderr) console.log(stderr)
+
+            if(stdout) console.log(stdout)
+        })
+
+        cp.exec(`type nul>${name}/views/signup.view.ejs`, (err, stderr, stdout) => {
+            if(err) throw err
+
+            if(stderr) console.log(stderr)
+
+            if(stdout) console.log(stdout)
+        })
+
+        cp.exec(`type nul>${name}/views/home.view.ejs`, (err, stderr, stdout) => {
+            if(err) throw err
+
+            if(stderr) console.log(stderr)
+
+            if(stdout) console.log(stdout)
+        })
+
+        cp.exec(`type nul>${name}/views/logs.view.ejs`, (err, stderr, stdout) => {
+            if(err) throw err
+
+            if(stderr) console.log(stderr)
+
+            if(stdout) console.log(stdout)
+        })
+
+        cp.exec(`type nul>${name}/views/dashboard.view.ejs`, (err, stderr, stdout) => {
+            if(err) throw err
+
+            if(stderr) console.log(stderr)
+
+            if(stdout) console.log(stdout)
+        })
+
+        cp.exec(`type nul>${name}/views/signin.view.ejs`, (err, stderr, stdout) => {
+            if(err) throw err
+
+            if(stderr) console.log(stderr)
+
+            if(stdout) console.log(stdout)
+        })
+
+        cp.exec(`type nul>${name}/app.js`, (err, stdout, stderr) => {
+            if(err) throw err
+
+            if(stderr) console.log(stderr)
+
+            if(stdout) console.log(stdout,
+                `****************************************\n`,
+                `#####Lets Switch up things a little#####\n`,
+                `****************************************\n`,
+                `Now Lets Do Magic using Hub to Access Github\n`,
+                `****************************************\n`)
+        })
+
+        cp.exec(`git init && git add .`, (err, stdout, stderr) => {
+            if(err) throw err
+
+            if(stderr) console.log(stderr)
+
+            if(stdout) console.log(stdout)
+        })
+
+        cp.exec(`git commit -m "Initial Commit, Using VillageWorkCLI"`, (err, stdout, stderr) => {
+            if(err) throw err
+
+            if(stderr) console.log(err)
+
+            if(stdout) console.log(stdout)
+        })
+
+        cp.exec(`hub create`, (err, stdout, stderr) => {
+            if(err) throw err
+
+            if(stderr) console.log(stderr)
+
+            if(stdout) console.log(stdout)
+        })
+
+        cp.exec(`git push origin`,  (err, stdout, stderr) => {
             if(err) throw err
     
-            if(stderr) console.log(`Error occured: \n`, stderr)
+            if(stderr) console.log(stderr)
             
             if(stdout) {
-                console.log(`Scaffolding: \n\x1b[33m`, stdout)
+                console.log(`Scaffolding: \n\x1b[33m`, stdout,
+                `****************************************\n`,
+                `WE Have Successfully Pushed The Project \n`,
+                `______________^(* _ *)^________________\n`,
+                `************* ENJOY!!! *****************\n`,
+                `************* HAPPY CODING *************`
+                )
                 websuccess(name)
             }
         })  
